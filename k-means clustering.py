0c7557d0189e7f805995cfd39e1a347e1ee1f18d
Antara10 <117644761+Antara10@users.noreply.github.com>
2023-05-08 01:39:12 +0530

    Update k-means clustering.py

--- k-means clustering.py
+++ k-means clustering.py
@@ -1,39 +1,58 @@
 
-from sklearn.cluster import KMeans
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_excel(r"C:\Users\bipro\OneDrive\Documents\Data Mining\Association Rule Mining\my k-means dataset.xlsx")
+def initialize_clusters(data, k):
+    # Randomly pick k data points as initial cluster centroids
+    centroids = data[np.random.choice(data.shape[0], k, replace=False), :]
+    return centroids
 
-df.head()
+def assign_points_to_clusters(data, centroids):
+    # Compute distances between each data point and the centroids
+    distances = np.sqrt(((data - centroids[:, np.newaxis])**2).sum(axis=2))
+    # Assign each data point to the closest cluster
+    cluster_assignment = np.argmin(distances, axis=0)
+    return cluster_assignment
 
-plt.scatter(df['AGE'], df['DATA MINING'])
+def update_clusters(data, k, cluster_assignment):
+    # Update each cluster's centroid to be the mean of all assigned data points
+    centroids = np.zeros((k, data.shape[1]))
+    for i in range(k):
+        centroids[i, :] = np.mean(data[cluster_assignment == i, :], axis=0)
+    return centroids
 
-km = KMeans(n_clusters = 6)
+def kmeans(data, k, max_iterations):
+    # Initialize cluster centroids
+    centroids = initialize_clusters(data, k)
+    # Iterate until convergence or maximum number of iterations reached
+    for i in range(max_iterations):
+        # Assign data points to nearest cluster
+        cluster_assignment = assign_points_to_clusters(data, centroids)
+        # Update centroids based on assigned data points
+        centroids = update_clusters(data, k, cluster_assignment)
+    return centroids, cluster_assignment
 
-y_predicted = km.fit_predict(df[['AGE', 'DATA STRUCTURE', 'MACHINE LEARNING', 'SOFTWARE ENGINEERING', 'DATA MINING']])
+# Get user input
+num_data_points = int(input("Enter the number of data points: "))
+num_features = int(input("Enter the number of features: "))
+k = int(input("Enter the number of clusters: "))
+max_iterations = int(input("Enter the maximum number of iterations: "))
 
-y_predicted 
+# Generate random data points
+data = np.random.randn(num_data_points, num_features)
 
-df['CLUSTER'] = y_predicted
+# Run K-Means clustering on data
+centroids, cluster_assignment = kmeans(data, k, max_iterations)
 
-df.head()
+# Plot the data points and cluster centroids
+colors = ['r', 'g', 'b', 'y', 'c', 'm']
+fig, ax = plt.subplots()
+for i in range(k):
+    points = data[cluster_assignment == i]
+    ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i%len(colors)])
+    ax.scatter(centroids[i, 0], centroids[i, 1], s=200, marker='*', c=colors[i%len(colors)])
+plt.title('K-Means Clustering')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.show()
 
-df1 = df[df.CLUSTER==0]
-df2 = df[df.CLUSTER==1]
-df3 = df[df.CLUSTER==2]
-df4 = df[df.CLUSTER==3]
-df5 = df[df.CLUSTER==4]
-df6 = df[df.CLUSTER==5]
-
-plt.scatter(df1.AGE, df1['DATA MINING'], color='green')
-plt.scatter(df2.AGE, df2['DATA MINING'], color='red')
-plt.scatter(df3.AGE, df3['DATA MINING'], color='blue')
-plt.scatter(df4.AGE, df4['DATA MINING'], color='yellow')
-plt.scatter(df5.AGE, df5['DATA MINING'], color='grey')
-plt.scatter(df6.AGE, df6['DATA MINING'], color='pink')
-
-plt.xlabel('Age')
-plt.ylabel('Marks')
-plt.legend()   
