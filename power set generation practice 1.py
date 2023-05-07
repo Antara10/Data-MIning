25499649f4938579977bde1929121c82d9886cac
Antara10 <117644761+Antara10@users.noreply.github.com>
2023-05-08 01:32:00 +0530

    Update power set generation practice 1.py

--- power set generation practice 1.py
+++ power set generation practice 1.py
@@ -1,16 +1,17 @@
-def power_set(input_set):
+def power_set(s):
     
-    result = [[]]
-    
-    for x in input_set:
-        
-        for subset in result[:]:
-            result.append(subset+[x])
-            
-    return result
+    if len(s) == 0:
+        return [[]]  # if s is an empty set, its power set is a set containing the empty set
+    else:
+        elem = s[0]
+        rest = s[1:]
+        ps = power_set(rest)
+        return ps + [[elem] + subset for subset in ps]
 
-input_set = set(input("enter input set seperated by space").split())
+user_input = input("Enter a set of elements separated by commas: ")
+s = user_input.split(",")  
 
-Output_set = power_set(input_set)
-
-print(Output_set)
+ps = power_set(s)
+print("Power set of", s, "is:")
+for subset in ps:
+    print(subset)
